c8c402d61401373b07d1bbce4315a6ffe375de20
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-30 15:04:59 -0300

    Create index.py

---
+++ aulas/aula004/ex001/index.py
@@ -0,0 +1,5 @@
+idade = input('Qual é a sua idade?\n')
+nome = input('Qual é o seu nome?\n')
+peso = input('Qual é o seu peso?\n')
+
+print(f'Seu nome é {nome}, você tem {idade} anos e pesa {peso} kg.')
