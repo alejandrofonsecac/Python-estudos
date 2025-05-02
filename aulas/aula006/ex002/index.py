ed7fde62bf2be01aae521e8240e897733bfe062a
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-01 21:18:30 -0300

    desafio003

---
+++ aulas/aula006/ex002/index.py
@@ -0,0 +1,4 @@
+n = float(input('Digite um valor: '))
+print(type(n))
+
+print(n)
---
+++ desafios/desafio003/index.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um numero: '))
+n2 = int(input('Digite um numero: '))
+s = n1 + n2
+
+print(f'A some entre {n1} e {n2} é: {s}')
+
+print(type(s))
