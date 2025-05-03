263565502b3259b5e6f2f0a4689d69f23a75ba7c
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 11:00:56 -0300

    desafio009

---
+++ exercicios e(ou) desafios/ex009 tabuada/index.py
@@ -0,0 +1,36 @@
+n1 = 0
+n2 = 0
+n3 = 0
+n4 = 0
+n5 = 0
+n6 = 0
+n7 = 0
+n8 = 0
+n9 = 0
+n10 = 0
+
+print('Me fale um numero e irei te dizer a tabuada de 1 a 10 dele.')
+
+n = int(input('Insira o numero: '))
+
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+
+print(f'{n} x 1 = {n1}\n')
+print(f'{n} x 2 = {n2}\n')
+print(f'{n} x 3 = {n3}\n')
+print(f'{n} x 4 = {n4}\n')
+print(f'{n} x 5 = {n5}\n')
+print(f'{n} x 6 = {n6}\n')
+print(f'{n} x 7 = {n7}\n')
+print(f'{n} x 8 = {n8}\n')
+print(f'{n} x 9 = {n9}\n')
+print(f'{n} x 10 = {n10}\n')
