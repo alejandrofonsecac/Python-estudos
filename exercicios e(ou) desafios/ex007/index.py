5f4d1ff5638c0f6befad1f08b04cfbe869d9bda6
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 10:32:45 -0300

    desafio007

---
+++ exercicios e(ou) desafios/ex007/index.py
@@ -0,0 +1,11 @@
+nt1 = 0
+nt2 = 0
+media = 0
+
+print('Me fale as 2 notas de um aluno que eu calcularei a média. \n')
+nt1 = int(input('Insira a primeira nota: '))
+nt2 = int(input('Insira a segunda nota: '))
+
+media = (nt1 + nt2) / 2
+
+print(f'A média é {media}')
