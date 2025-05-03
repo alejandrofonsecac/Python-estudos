4cf092be2ebfcfff148209459f41e2c07c9ec9a1
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 10:23:08 -0300

    desafio005

---
+++ exercicios e(ou) desafios/ex005/index.py
@@ -0,0 +1,9 @@
+n = 0
+
+print('Me fale um numero e eu le mostrarei o sucessor e antecessor dele.\n')
+n = int(input('Digite o número: '))
+
+suc = n + 1
+ant = n - 1
+
+print(f'O antecessor é {ant} e o sucessor é o {suc}.')
