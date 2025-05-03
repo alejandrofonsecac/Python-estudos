5741f43782079e5b4fef5ae88de8cefb027e9927
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 10:26:54 -0300

    desafio006

---
+++ exercicios e(ou) desafios/ex006/index.py
@@ -0,0 +1,13 @@
+n = 0
+dobro = 0
+triplo = 0
+raizquad = 0
+
+print('Me diga um número e eu lhe direi o dobro, o triplo e a raiz quadrada')
+n = int(input('Me diga o número: '))
+
+dobro = n * 2
+triplo = n * 3
+raizquad = n **2
+
+print(f'O drobro do seu número é {dobro}, o triplo é {triplo} e a raiz quadrada é {raizquad}')
