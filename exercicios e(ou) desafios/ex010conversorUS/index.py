3b05da90c5d13e9d13e143e2f645858b7778ba96
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 11:08:43 -0300

    desafio010

---
+++ exercicios e(ou) desafios/ex010conversorUS/index.py
@@ -0,0 +1,9 @@
+valorRS = 0
+valorUSS = 0
+
+print('Digite o valor em R$ e transformarei em U$$\n')
+valorRS = int(input(print('Insira o valor:')))
+
+valorUSS = valorRS * 3.27
+
+print(f'O valor convertido é: {valorUSS}U$$')
