5fb594590d945e69d97cb8afe1aed3979d0360fb
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-05-03 10:41:38 -0300

    desafio008

---
+++ exercicios e(ou) desafios/ex008/index.py
@@ -0,0 +1,8 @@
+n = 0
+nconv = 0
+
+n = int(input(print('Me diga um valor em metros e transformarei em centimetros.')))
+
+nconv = n * 100
+
+print(f'O valor que você insiriu é {n} metros e o valor convertido em centimetros é {nconv} centimetros.')
