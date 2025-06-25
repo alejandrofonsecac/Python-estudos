7f998e0769e393981c626ceb5814765326419a40
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-06-25 11:04:28 -0300

    Create index.py

---
+++ aulas/aula008/B/index.py
@@ -0,0 +1,6 @@
+import random
+num = random.randint(1, 10)  # Gera um inteiro entre 1 e 10 (incluindo ambos)
+print(num)
+
+#random.randint(1, 10) gera numero inteiro
+#random.uniform(1, 10) gera numero float
