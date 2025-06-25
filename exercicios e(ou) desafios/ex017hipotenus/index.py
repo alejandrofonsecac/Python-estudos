2aa653df992996b786827f8c32cf8f3ead8d26b0
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-06-25 12:34:42 -0300

    Update index.py

--- exercicios e(ou) desafios/ex017hipotenus/index.py
+++ exercicios e(ou) desafios/ex017hipotenus/index.py
@@ -2,9 +2,9 @@ from math import sqrt
 
 hipo = 0
 x = 0
-catop = float(input(print('Insira o cateto oposto do triangulo retangulo')))
-catad = float(input(print('Insira o cateto adajacente do triangulo retangulo')))
+catop = float(input('Insira o cateto oposto do triangulo retangulo: '))
+catad = float(input('Insira o cateto adajacente do triangulo retangulo: '))
 
 x = catop**2 + catad**2
 hipo = sqrt(x)
-print(f'A hipotenusa é: {hipo}')
+print(f'A hipotenusa é: {hipo:.2f}')
