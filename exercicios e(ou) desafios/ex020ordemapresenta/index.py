9d47bb058a3e3e27ff0f065274b8cb78e63a02a8
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-06-26 14:48:30 -0300

    Update index.py

--- exercicios e(ou) desafios/ex020ordemapresenta/index.py
+++ exercicios e(ou) desafios/ex020ordemapresenta/index.py
@@ -6,7 +6,6 @@ aluno2 = input('\nInsira o nome do 2 aluno: ')
 aluno3 = input('\nInsira o nome do 3 aluno: ')
 aluno4 = input('\nInsira o nome do 4 aluno: ')
 
-if ordem == 1:
-    print(f'A ordem será: {aluno1}, {aluno2},{aluno3},{aluno4}')
-elif ordem == 2:
-    print(f'A ordem será: {aluno4}, {aluno3},{aluno2},{aluno1}')
+lista = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(lista)
+print(lista)
