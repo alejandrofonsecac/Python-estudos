632585ddad4da7990a37710016aab3d22b17fa08
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-06-26 14:53:27 -0300

    Update index.py

--- exercicios e(ou) desafios/ex019numaleatorio/index.py
+++ exercicios e(ou) desafios/ex019numaleatorio/index.py
@@ -1,13 +1,3 @@
 import random
 num = random.randint(1, 4)
 
-print('Tem 4 alunos, os nomes deles são, nesta sequencia: \n Alejandro \n Mateus \n Wesley \n João \n Os escolhidos para limpar o quadro são: ')
-
-if num == 1:
-    print('\nAlejandro será o escolhido')
-elif num == 2:
-    print('\nMateus será o escolhido')
-elif num == 3:
-    print('\nWesley será o escolhido')
-elif num == 4:
-    print('\nJoão será o escolhido')
