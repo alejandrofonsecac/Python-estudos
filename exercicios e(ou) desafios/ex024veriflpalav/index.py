34386403f1f020ec76cee35ac9875b0b27877b2f
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-06-28 15:28:47 -0300

    Create index.py

---
+++ exercicios e(ou) desafios/ex024veriflpalav/index.py
@@ -0,0 +1,6 @@
+cidade = input('Qual o nome da sua cidade? ').strip().upper()
+
+if 'SANTO' in cidade:
+    print('Sua cidade tem a palavra SANTO')
+else: 
+    print('Sua cidade não tem a palavra SANTO')
